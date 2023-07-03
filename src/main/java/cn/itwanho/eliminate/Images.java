6445f3abd0e3f3b745b1128ad75e1a3aea06f0e2
orange-ora <941418720@qq.com>
2023-07-03 09:43:34 +0800

    图片加载

---
+++ src/main/java/cn/itwanho/eliminate/Images.java
@@ -0,0 +1,29 @@
+package cn.itwanho.eliminate;
+
+import javax.swing.*;
+
+/**
+ * 图片类
+ */
+public class Images {
+    public static ImageIcon background; // 背景图
+    public static ImageIcon bear; // 小熊图
+    public static ImageIcon bird; // 小鸟图
+    public static ImageIcon fox; // 狐狸图
+    public static ImageIcon frog; // 青蛙
+    public static ImageIcon[] bombs; // 爆炸图数组
+
+    // 加载图片
+    static {
+        background = new ImageIcon("img/background.png");
+        bear = new ImageIcon("img/bear.png");
+        bird = new ImageIcon("img/bird.png");
+        fox = new ImageIcon("img/fox.png");
+        frog = new ImageIcon("img/frog.png");
+        bombs = new ImageIcon[4];
+        for (int i = 0; i < bombs.length; i++) {
+            bombs[i] = new ImageIcon("img/bom" + (i + 1) + ".png");
+        }
+    }
+
+}
